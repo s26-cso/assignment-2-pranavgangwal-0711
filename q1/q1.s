acc6e4673eb18c9fd3437632bc46e426a0f043b5
Pranav Gangwal <pranav.gangwal@gmail.com>
2026-04-14 18:49:29 +0530

    3rd Attempt

--- q1/q1.s
+++ q1/q1.s
@@ -5,27 +5,27 @@
 
 make_node:
     addi sp,sp,-16
-    sw ra,12(sp)
-    sw s0,8(sp)
+    sd ra,8(sp)
+    sd s0,0(sp)
     mv s0,a0              # save val in s0
     li a0,12              # allocate 12 bytes for node
     call malloc
     sw s0,0(a0)           # node->val=val
-    sw x0,4(a0)           # node->left=NULL
-    sw x0,8(a0)           # node->right=NULL
-    lw ra,12(sp)
-    lw s0,8(sp)
+    sd x0,8(a0)           # node->left=NULL
+    sd x0,16(a0)           # node->right=NULL
+    ld ra,8(sp)
+    ld s0,0(sp)
     addi sp,sp,16
     ret
 
 insert:
-    addi sp,sp,-16
-    sw ra,12(sp)
-    sw s0,8(sp)
-    sw s1,4(sp)
+    addi sp,sp,-32
+    sd ra,24(sp)
+    sd s0,16(sp)
+    sd s1,8(sp)
     mv s0,a0                            # s0=root
     mv s1,a1                            # s1=val
-    bne s0,x0,insert_notnull          # if root==NULL make new node and return
+    bne s0,x0,insert_notnull            # if root==NULL make new node and return
     mv a0,s1
     call make_node
     j insert_done
@@ -33,23 +33,23 @@ insert:
         lw t0,0(s0)                     # t0=root->val
         beq s1,t0,insert_return_root    # if val==root->val do nothing
         blt s1,t0,insert_left           # val < root->val go left
-        lw a0,8(s0)                     # a0=root->right
+        ld a0,16(s0)                     # a0=root->right
         mv a1,s1       
         call insert
-        sw a0,8(s0)                     # root->right=result
+        sd a0,16(s0)                     # root->right=result
         j insert_return_root
         insert_left:
-            lw a0,4(s0)                 # a0=root->left
+            ld a0,8(s0)                 # a0=root->left
             mv a1,s1
             call insert
-            sw a0,4(s0)                 # root->left=result
+            sd a0,8(s0)                 # root->left=result
     insert_return_root:
         mv a0,s0                        # a0=root
     insert_done:
-        lw ra,12(sp)
-        lw s0,8(sp)
-        lw s1,4(sp)
-        addi sp,sp,16
+        ld ra,24(sp)
+        ld s0,16(sp)
+        ld s1,8(sp)
+        addi sp,sp,32
         ret
 
 get:
@@ -57,20 +57,20 @@ get:
     lw t0,0(a0)
     beq a1,t0,get_done                 # return root if root->val==val
     blt a1,t0,get_go_left              # go left val < root->val
-    lw a0,8(a0)
+    ld a0,16(a0)
     j get
     get_go_left:
-        lw a0,4(a0)
+        ld a0,8(a0)
         j get
     get_done:
         ret
 
 getAtMost:
-    addi sp,sp,-16
-    sw ra,12(sp)
-    sw s0,8(sp)
-    sw s1,4(sp)
-    sw s2,0(sp)
+    addi sp,sp,-32
+    sd ra,24(sp)
+    sd s0,16(sp)
+    sd s1,8(sp)
+    sd s2,0(sp)
     mv s0,a0                            # s0=val
     mv s1,a1                            # s1=root
     li s2,-1                            # s2=best result so far
@@ -80,16 +80,16 @@ getAtMost:
         blt s0,t0,getAtMost_go_left     # if val < root->val go left
             mv s2,t0                    # update best
             beq s0,t0,getAtMost_done    # exact match, can't do better
-            lw s1,8(s1)                 # root=root->right
+            ld s1,16(s1)                 # root=root->right
             j getAtMost_loop            # trying to find if a better answer exists
         getAtMost_go_left:
-            lw s1,4(s1)                 # root=root->left
+            ld s1,8(s1)                 # root=root->left
             j getAtMost_loop   
     getAtMost_done:
         mv a0,s2                        # return best result
-        lw ra,12(sp)
-        lw s0,8(sp)
-        lw s1,4(sp)
-        lw s2,0(sp)
-        addi sp,sp,16
+        ld ra,24(sp)
+        ld s0,16(sp)
+        ld s1,8(sp)
+        ld s2,0(sp)
+        addi sp,sp,32
         ret
